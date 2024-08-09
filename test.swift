b7300ccdc479a8b095727370d8cd65c829667e3e
sharuban <s.s.ruban98@gmail.com>
2024-08-09 08:23:57 +0530

    test changes

---
+++ test.swift
@@ -0,0 +1,29 @@
+
+
+
+print ( " Hello world ! " )
+
+print ( " Some other text " )
+
+// Some important point about next line 
+
+print ( " ewgogjgoiggoiqeqdifgggeugdsjffjgofffer " )
+
+var FristVarible = " Some Text "
+
+let FristConstant = " Some Text in constant " 
+
+print ( FristVarible )
+
+print ( FristConstant )
+
+FristVarible = " Some other Text "
+
+// FristConstant = " constant is constant " 
+// let is constant cannot assign to value change let to var to make mutable 
+
+print ( FristVarible )
+
+print ( FristConstant )
+
+
